cb5722d30449adb6d919873e41efb1c61fc775bc
So00 <atourner@student.42.fr>
2018-10-28 00:29:58 +0200

    draggable image for all

--- view/js/takingPic.js
+++ view/js/takingPic.js
@@ -1,6 +1,6 @@
 function init() {
     navigator.mediaDevices.getUserMedia({ audio: false, video: { width: 800, height: 600 } }).then(function(mediaStream) {
-
+        
         var video = document.getElementById('sourcevid');
         video.height = document.body.clientHeight / 1.5;
         video.width = video.height * 1.34;
@@ -32,10 +32,10 @@ function add_answer(xhr)
 }
 
 {
-var filter = document.getElementsByClassName("filter");
-for (var i = 0; i < filter.length; i++)
-{
-    filter[i].addEventListener('click', function (e){
+    var filter = document.getElementsByClassName("filter");
+    for (var i = 0; i < filter.length; i++)
+    {
+        filter[i].addEventListener('click', function (e){
             var act = e.target.cloneNode();
             video = document.body.querySelector("#vidContainer");
             var nb_filter = document.getElementsByClassName("activeFilter").length;
@@ -45,70 +45,70 @@ for (var i = 0; i < filter.length; i++)
             act.style.left = "0";
             act.className = "activeFilter filter_" + nb_filter;
             video.appendChild(act);
-
-            var dragItem = document.querySelector(".activeFilter");
+            
+            var dragItem = document.querySelector(".filter_" + nb_filter);
             var container = document.querySelector("#vidContainer");
-
-    var active = false;
-    var currentX;
-    var currentY;
-    var initialX;
-    var initialY;
-    var xOffset = 0;
-    var yOffset = 0;
-
-    container.addEventListener("touchstart", dragStart, false);
-    container.addEventListener("touchend", dragEnd, false);
-    container.addEventListener("touchmove", drag, false);
-
-    container.addEventListener("mousedown", dragStart, false);
-    container.addEventListener("mouseup", dragEnd, false);
-    container.addEventListener("mousemove", drag, false);
-
-    function dragStart(e) {
-      if (e.type === "touchstart") {
-        initialX = e.touches[0].clientX - xOffset;
-        initialY = e.touches[0].clientY - yOffset;
-      } else {
-        initialX = e.clientX - xOffset;
-        initialY = e.clientY - yOffset;
-      }
-
-      if (e.target === dragItem) {
-        active = true;
-      }
-    }
-
-    function dragEnd(e) {
-      initialX = currentX;
-      initialY = currentY;
-
-      active = false;
-    }
-
-    function drag(e) {
-      if (active) {
-      
-        e.preventDefault();
-      
-        if (e.type === "touchmove") {
-          currentX = e.touches[0].clientX - initialX;
-          currentY = e.touches[0].clientY - initialY;
-        } else {
-          currentX = e.clientX - initialX;
-          currentY = e.clientY - initialY;
-        }
-
-        xOffset = currentX;
-        yOffset = currentY;
-
-        setTranslate(currentX, currentY, dragItem);
-      }
-    }
-
-    function setTranslate(xPos, yPos, el) {
-      el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
-    }
+            
+            var active = false;
+            var currentX;
+            var currentY;
+            var initialX;
+            var initialY;
+            var xOffset = 0;
+            var yOffset = 0;
+            
+            container.addEventListener("touchstart", dragStart, false);
+            container.addEventListener("touchend", dragEnd, false);
+            container.addEventListener("touchmove", drag, false);
+            
+            container.addEventListener("mousedown", dragStart, false);
+            container.addEventListener("mouseup", dragEnd, false);
+            container.addEventListener("mousemove", drag, false);
+            
+            function dragStart(e) {
+                if (e.type === "touchstart") {
+                    initialX = e.touches[0].clientX - xOffset;
+                    initialY = e.touches[0].clientY - yOffset;
+                } else {
+                    initialX = e.clientX - xOffset;
+                    initialY = e.clientY - yOffset;
+                }
+                
+                if (e.target === dragItem) {
+                    active = true;
+                }
+            }
+            
+            function dragEnd(e) {
+                initialX = currentX;
+                initialY = currentY;
+                
+                active = false;
+            }
+            
+            function drag(e) {
+                if (active) {
+                    
+                    e.preventDefault();
+                    
+                    if (e.type === "touchmove") {
+                        currentX = e.touches[0].clientX - initialX;
+                        currentY = e.touches[0].clientY - initialY;
+                    } else {
+                        currentX = e.clientX - initialX;
+                        currentY = e.clientY - initialY;
+                    }
+                    
+                    xOffset = currentX;
+                    yOffset = currentY;
+                    
+                    setTranslate(currentX, currentY, dragItem);
+                }
+            }
+            
+            function setTranslate(xPos, yPos, el) {
+                el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+            }
         }
     )
 }
@@ -125,7 +125,7 @@ function clone(){
     newEl.drawImage(vivi, 0,0, 700, 500);
     var base64 = toAdd.toDataURL("image/png");
     var xhr=null;
- 
+    
     if (window.XMLHttpRequest) {
         xhr = new XMLHttpRequest();
     }
@@ -133,9 +133,9 @@ function clone(){
     {
         xhr = new ActiveXObject("Microsoft.XMLHTTP");
     }
-
+    
     xhr.onreadystatechange = function() {add_answer(xhr);}
- 
+    
     xhr.open("POST", "http://localhost/controler/add_picture_ajax.php", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.send("picture="+base64);
@@ -146,20 +146,20 @@ document.getElementById('upload_pic').addEventListener('submit', function (e){
     var data = document.getElementById('picData').files[0];
     var xhr=null;
     var formData = new FormData();
-
+    
     formData.append("picture", data)
-        if (window.XMLHttpRequest) {
-            xhr = new XMLHttpRequest();
-        }
-        else if (window.ActiveXObject);
-        {
-            xhr = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-
-        xhr.onreadystatechange = function() {add_answer(xhr);}
-
-        xhr.open("POST", "http://localhost/controler/add_picture_ajax.php", true);
-        xhr.send(formData);
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    }
+    else if (window.ActiveXObject);
+    {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    
+    xhr.onreadystatechange = function() {add_answer(xhr);}
+    
+    xhr.open("POST", "http://localhost/controler/add_picture_ajax.php", true);
+    xhr.send(formData);
 });
 
 window.onload = init();
